3cfe7f3347b81d3a917d2a0d19be49dbb245d166
Josias NTUAREMBA <josiasdan@hotmail.fr>
2018-06-14 10:06:40 +0200

    Fin de Exercice Date - Time Java 8

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,13 +20,13 @@ public class DateAndTime_07_Test {
     public void test_date_to_localdate() throws Exception {
 
         // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+        Date date = new Date(117,1,12);
 
         // TODO transformer la date en Instant
 
         // TODO transformer la date en LocalDate
-
-        LocalDate result = null;
+        
+        LocalDate result = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();;
 
         // TODO valoriser les différentes variables afin de rendre le test passant
 
